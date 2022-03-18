e5cf521a0b05686eeaded27eeab3c8545696ce3d
dgfghsjd <dgfghsjd@naver.com>
2022-03-18 18:59:11 +0900

    [B/E_EducationMVP]add service and router layer

--- back/src/db/index.js
+++ back/src/db/index.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import { User } from "./models/User";
+import { Education } from "./models/Education";
 
 const DB_URL =
   process.env.MONGODB_URL ||
@@ -16,3 +17,4 @@ db.on("error", (error) =>
 );
 
 export { User };
+export { Education };
