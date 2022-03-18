258671badd025121d50b9629952e50297513a059
mungsik <you@example.com>
2022-03-18 17:43:16 +0900

    EducationAdd, EducationEditForm 작업 중

--- front/src/components/education/EducationAdd.js
+++ front/src/components/education/EducationAdd.js
@@ -1,3 +1,24 @@
-function EducationAdd() {}
+import React, { useState } from "react";
+import * as Api from "../../api";
+
+function EducationAdd({ portfolioOwnerId, setIsAdding, setEducationList }) {
+
+  const [school, setSchool] = useState("");
+  const [major, setMajor] = useState("");
+  const [position, setPosition] = useState("");
+
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+
+    const user_id = portfolioOwnerId;
+
+    await Api.post("education/create", {
+      user_id,
+      school,
+      major,
+      position,
+    });
+  }
+}
 
 export default EducationAdd;
