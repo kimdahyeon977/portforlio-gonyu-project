bcbc35b744dfca1a9573ad691992fc55b2374519
hajun <audgkwns44@gmail.com>
2022-03-20 09:54:35 +0900

    Basic code structure of AwardCard

--- front/src/components/award/AwardCard.js
+++ front/src/components/award/AwardCard.js
@@ -1,9 +1,26 @@
-import React, {useState} from "react";
+import React from "react";
+import {Card, Row} from "react-bootstrap"
 
 function AwardCard(){
-    return <>
-        <p>this one is mock-up AwardCard</p>
-    </>
+    return <Card className="mb-2 ms-3 mr-5" xs={1} sm={2}>
+        <Card.Body>
+            <Card.Title>
+                <Row className="justify-content-md-left">
+                    수상이력
+                </Row>
+            </Card.Title>
+            <Card.Subtitle>
+                <Row>
+                    subtitle?
+                </Row>
+            </Card.Subtitle>
+            <Card.Text>
+                <Row style={{color: "grey"}}>
+                    Text
+                </Row>
+            </Card.Text>
+        </Card.Body>
+    </Card>
 }
 
 export default AwardCard;
