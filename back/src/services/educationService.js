01ba2dac63bef4af1a888aeca6633eacd1e76b81
dgfghsjd <dgfghsjd@naver.com>
2022-03-23 05:36:10 +0900

    add error handling

--- back/src/services/educationService.js
+++ back/src/services/educationService.js
@@ -3,7 +3,7 @@ import { Education } from "../db";
 import { v4 as uuidv4 } from "uuid";
 
 class EducationService {
-  static async addEducation({ userId, school, major, position }) {
+  async addEducation({ userId, school, major, position }) {
     // id로 유니크 값 사용
     const id = uuidv4();
 
@@ -14,77 +14,89 @@ class EducationService {
     return createdNewEducation;
   }
 
-  static async getEducation({ educationId }) {
+  async getEducation({ educationId }) {
     // 해당 id를 가진 데이터가 db에 존재 여부 확인
-    const education = await Education.findById({ educationId });
-    if (!education) {
-      throw new Error(
-        "해당 id를 가진 학력 데이터는 없습니다. 다시 한 번 확인해 주세요."
-      );
-    }
+    try {
+      const education = await Education.findById({ educationId });
+      if (!education) {
+        throw new Error(
+          "해당 id를 가진 학력 데이터는 없습니다. 다시 한 번 확인해 주세요."
+        );
+      }
 
-    return education;
+      return education;
+    } catch (error) {
+      next(error);
+    }
   }
 
-  static async getEducationList({ userId }) {
+  async getEducationList({ userId }) {
     const educations = await Education.findByUserId({ userId });
     return educations;
   }
 
-  static async setEducation({ educationId, toUpdate }) {
-    let education = await Education.findById({ educationId });
+  async setEducation({ educationId, toUpdate }) {
+    try {
+      let education = await Education.findById({ educationId });
 
-    // db에서 찾지 못한 경우, 에러 메시지 반환
-    if (!education) {
-      throw new Error(
-        "해당 id를 가진 학력 데이터는 없습니다. 다시 한 번 확인해 주세요."
-      );
+      // db에서 찾지 못한 경우, 에러 메시지 반환
+      if (!education) {
+        throw new Error(
+          "해당 id를 가진 학력 데이터는 없습니다. 다시 한 번 확인해 주세요."
+        );
+      }
+
+      if (toUpdate.school) {
+        const fieldToUpdate = "school";
+        const newValue = toUpdate.school;
+        education = await Education.update({
+          educationId,
+          fieldToUpdate,
+          newValue,
+        });
+      }
+
+      if (toUpdate.major) {
+        const fieldToUpdate = "major";
+        const newValue = toUpdate.major;
+        education = await Education.update({
+          educationId,
+          fieldToUpdate,
+          newValue,
+        });
+      }
+
+      if (toUpdate.position) {
+        const fieldToUpdate = "position";
+        const newValue = toUpdate.position;
+        education = await Education.update({
+          educationId,
+          fieldToUpdate,
+          newValue,
+        });
+      }
+
+      return education;
+    } catch (error) {
+      next(error);
     }
-
-    if (toUpdate.school) {
-      const fieldToUpdate = "school";
-      const newValue = toUpdate.school;
-      education = await Education.update({
-        educationId,
-        fieldToUpdate,
-        newValue,
-      });
-    }
-
-    if (toUpdate.major) {
-      const fieldToUpdate = "major";
-      const newValue = toUpdate.major;
-      education = await Education.update({
-        educationId,
-        fieldToUpdate,
-        newValue,
-      });
-    }
-
-    if (toUpdate.position) {
-      const fieldToUpdate = "position";
-      const newValue = toUpdate.position;
-      education = await Education.update({
-        educationId,
-        fieldToUpdate,
-        newValue,
-      });
-    }
-
-    return education;
   }
 
-  static async deleteEducation({ educationId }) {
-    const isDataDeleted = await Education.deleteById({ educationId });
+  async deleteEducation({ educationId }) {
+    try {
+      const isDataDeleted = await Education.deleteById({ educationId });
 
-    // db에서 찾지 못한 경우, 에러 메시지 반환
-    if (!isDataDeleted) {
-      throw new Error(
-        "해당 id를 가진 학력 데이터는 없습니다. 다시 한 번 확인해 주세요. 데이터가 지워지지 않았습니다."
-      );
+      // db에서 찾지 못한 경우, 에러 메시지 반환
+      if (!isDataDeleted) {
+        throw new Error(
+          "해당 id를 가진 학력 데이터는 없습니다. 다시 한 번 확인해 주세요. 데이터가 지워지지 않았습니다."
+        );
+      }
+
+      return isDataDeleted;
+    } catch (error) {
+      next(error);
     }
-
-    return isDataDeleted;
   }
 }
 
