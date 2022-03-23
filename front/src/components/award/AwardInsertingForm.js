5bfe48a9062f278fe485245cebca32ea23de3540
hajun <audgkwns44@gmail.com>
2022-03-24 08:23:50 +0900

    AwardInsertingForm Cleaning code

--- front/src/components/award/AwardInsertingForm.js
+++ front/src/components/award/AwardInsertingForm.js
@@ -21,43 +21,44 @@ function AwardInsertingForm({setIsInserting, ownerId, setAwards}){
         setIsInserting(false)
     }
 
-    return <Form onSubmit={submitHandler}>
-                <Form.Group controlId="formBasicTitle" className="mb-3">
-                    <Form.Label>수상한 상의 이름</Form.Label>
-                    <Form.Control 
-                        type="text"
-                        value={title} 
-                        placeholder="받으신 상의 이름이 어떻게 되나요?" 
-                        onChange={(event) => setTitle(event.target.value)}
-                    />
-                    
-                </Form.Group>
-
-                <Form.Group controlId="formBasicDescription" className="mb-3">
-                    <Form.Label>수상한 상에 대한 설명</Form.Label>
-                    <Form.Control 
-                        type="text" 
-                        value={description} 
-                        placeholder="뭐에 대한 상인가요?"
-                        onChange={(event) => setDescription(event.target.value)}
-                    />
-                </Form.Group>
+    return (
+        <Form onSubmit={submitHandler}>
+            <Form.Group controlId="formBasicTitle" className="mb-3">
+                <Form.Label>수상한 상의 이름</Form.Label>
+                <Form.Control 
+                    type="text"
+                    value={title} 
+                    placeholder="받으신 상의 이름이 어떻게 되나요?" 
+                    onChange={(event) => setTitle(event.target.value)}
+                />
                 
-                <Form.Group className="text-center">
-                    <Col>
-                        <Button className="me-3" variant="primary" type="submit">
-                            나는 이 상을 받아따! (확인)
-                        </Button>
-                        <Button variant="danger" onClick={() => {
-                                setIsInserting(false)
-                                console.log('CANCEL BUTTON HAS BEEN CLICKED')
-                            }
-                        }> 
-                            사실 못 받았다.. (취소)
-                        </Button>
-                    </Col>
-                </Form.Group>
-            </Form>
+            </Form.Group>
+
+            <Form.Group controlId="formBasicDescription" className="mb-3">
+                <Form.Label>수상한 상에 대한 설명</Form.Label>
+                <Form.Control 
+                    type="text" 
+                    value={description} 
+                    placeholder="뭐에 대한 상인가요?"
+                    onChange={(event) => setDescription(event.target.value)}
+                />
+            </Form.Group>
+            
+            <Form.Group className="text-center">
+                <Col>
+                    <Button className="me-3" variant="primary" type="submit">
+                        나는 이 상을 받아따! (확인)
+                    </Button>
+                    <Button variant="danger" onClick={() => {
+                            setIsInserting(false)
+                        }
+                    }> 
+                        사실 못 받았다.. (취소)
+                    </Button>
+                </Col>
+            </Form.Group>
+        </Form>
+    )
 }
 
 export default AwardInsertingForm;
