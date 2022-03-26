d83dbab7195c95bf281219bebcc70ea3bb71548d
gitlabkimdahyeon <gitlab>
2022-03-26 16:17:50 +0900

    구글소셜로그인 기능

---
+++ front/src/components/user/Google.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { GoogleLogin } from 'react-google-login';
+import styled from 'styled-components';
+
+class Login extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            id: '',
+            name: '',
+            provider: '',
+        }
+    }
+    // Google Login
+    responseGoogle = (res) => {
+        console.log(res)
+    }
+
+    // Login Fail
+    responseFail = (err) => {
+        console.error(err);
+    }
+
+    render() {
+        return (
+            <Container>
+                <GoogleLogin
+                    clientId={"557898764993-m4919nhfs6d07uspbtianuh6ilfr60n4.apps.googleusercontent.com"}
+                    buttonText="Google"
+                    onSuccess={this.responseGoogle}
+                    onFailure={this.responseFail}
+                />
+            </Container>
+        );
+    }
+}
+
+const Container = styled.div`
+    display: flex;
+    flex-flow: column wrap;
+`
+
+
+export default Login;
