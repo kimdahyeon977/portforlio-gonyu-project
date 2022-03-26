167b3982d6a5717c1721484f90cf7c998fdfde40
dgfghsjd <dgfghsjd@naver.com>
2022-03-26 15:12:37 +0900

    apply nanoid

--- back/src/db/schemas/award.js
+++ back/src/db/schemas/award.js
@@ -1,25 +1,22 @@
 import { Schema, model } from "mongoose";
 
-const id=require('./types/id.js')
+const id = require("./types/id.js");
 
-const AwardSchema = new Schema(
-  {
-    id,
-    userId: {
-      type: String,
-      required: true,
-    },
-    title: {
-      type: String,
-      required: true,
-    },
-    description: {
-      type: String,
-      required: true,
-    },
+const AwardSchema = new Schema({
+  id,
+  userId: {
+    type: String,
+    required: true,
   },
-
-);
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+});
 
 const AwardModel = model("Award", AwardSchema);
 
--- back/src/db/schemas/project.js
+++ back/src/db/schemas/project.js
@@ -1,28 +1,32 @@
 import { Schema, model } from "mongoose";
-const id=require('./types/projectId.js')//projectId가 자동으로 생성되서 사용할 수 있다.
+const id = require("./types/id.js"); //projectId가 자동으로 생성되서 사용할 수 있다.
 const projectSchema = Schema(
   {
     id, //프로젝트 고유아이디
-    userId:{ //로그인한 유저이름
+    userId: {
+      //로그인한 유저이름
       type: String,
       required: true,
     },
-    title: { //프로젝트 이름
+    title: {
+      //프로젝트 이름
       type: String,
       required: true,
     },
-    task: { //어떤 업무수행했는지
+    task: {
+      //어떤 업무수행했는지
       type: String,
       required: true,
     },
-    fromDate: { //프로젝트 수행 날짜
+    fromDate: {
+      //프로젝트 수행 날짜
       type: Date,
       required: true,
     },
     toDate: {
       type: Date,
       required: true,
-    }
+    },
   },
   {
     timestamps: true,
@@ -32,4 +36,3 @@ const projectSchema = Schema(
 const ProjectModel = model("Project", projectSchema);
 
 export { ProjectModel };
-
