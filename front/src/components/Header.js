dff9600d07ded383d2bb9693bb290ea3bad1da8c
hajun <audgkwns44@gmail.com>
2022-03-27 07:39:35 +0900

    testing dark mode

--- front/src/components/Header.js
+++ front/src/components/Header.js
@@ -30,13 +30,24 @@ function Header() {
       style={{ backgroundColor: isDarkMode ? 'black' : 'white' }}
     >
       <Nav.Item className="me-auto mb-5">
-        <Nav.Link disabled>안녕하세요, 포트폴리오 공유 서비스입니다.</Nav.Link>
+        <Nav.Link
+          disabled
+          style={{ color: isDarkMode ? '#DDD' : '#333'}}
+        >안녕하세요, 포트폴리오 공유 서비스입니다.
+        </Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link onClick={() => navigate('/')}>나의 페이지</Nav.Link>
+        <Nav.Link 
+          onClick={() => navigate('/')}
+          style={{backgroundColor : isDarkMode ? '#CCC' : "#444"}}
+        >나의 페이지
+        </Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link onClick={() => navigate('/network')}>네트워크</Nav.Link>
+        <Nav.Link
+          onClick={() => navigate('/network')}
+          style={{backgroundColor : isDarkMode ? '#CCC' : "#444"}}
+        >네트워크</Nav.Link>
       </Nav.Item>
       {isLogin && (
         <Nav.Item>
